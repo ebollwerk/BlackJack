87241085182d3379d5c338213b9ab2ab63a740de
ebollwerk <ebollwerk@gmail.com>
2016-07-04 11:14:38 -0400

    Add steps through first deal

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python
 
 # define main function
+# assign point values to cards
 # shuffle the deck
+# deal to player 1
+# deal to dealer
+# count cards for player 1
+# count cards for dealer
+# if card sum for player 1 <10 "hit"
+# if card sum for player 1 >19 "stand"
+# if card sum for dealer =< 17 "hit"
+# if card sum for dealer => 18 "stand"
+
 
 if __name__ == '__main__':
     main()
